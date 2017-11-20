49faec0bb3dc4650b0b73397cc4be9b2c3b2e214
jenzax <30765989+jenzax@users.noreply.github.com>
2017-11-20 23:23:46 +1100

    Import Project

---
+++ problem 1 Multiples of 3 and 5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem_1_Multiples_of_3_and_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> list = Enumerable.Range(1, 999);
+            int a = 0;
+            foreach (int z in list)
+            {
+                if (z % 3 == 0 || z % 5 == 0)
+                {
+                    Console.WriteLine(z);
+                    a = a + z;
+                }
+
+            }
+            Console.WriteLine(a);
+
+            
+        }
+    }
+}
